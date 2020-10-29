e64fb34122bdde02d45fb960c837ac9b09935187
jroldaro <jacinto.roldan.rodriguez@everis.com>
2020-10-29 16:48:06 +0100

    animations in resume

--- js/main.js
+++ js/main.js
@@ -8,9 +8,17 @@ const logoHeader = document.querySelector(".logo-header");
 const socialIconsLinks = document.querySelector(".social-icons-links");
 const socialIconsButton = document.querySelector(".social-icons__button");
 
+var isLogoVisible = false;
+const tabs =  ["profile", "resume", "portfolio", "contact"];
+var tabActive = tabs[0];
+
 const profileButton = document.querySelectorAll(".menu-main_tab-link")[0];
+const resumeButton = document.querySelectorAll(".menu-main_tab-link")[1];
+const protfolioButton = document.querySelectorAll(".menu-main_tab-link")[2];
+const contactButton = document.querySelectorAll(".menu-main_tab-link")[3];
 
 const profileContent = document.querySelector(".profile");
+    const profileContentProgressBars = document.querySelectorAll(".resume-skills-data__line-progressbar");
 const resumeContent = document.querySelector(".resume");
 const portfolioContent = document.querySelector(".portfolio");
 const contactContent = document.querySelector(".contact");
@@ -18,12 +26,83 @@ const contactContent = document.querySelector(".contact");
 /* FUNCTIONS */
 function initPage(){
     initButtons();
+    initProgressBars();
 }
 
 function initButtons(){
-    socialIconsButton.addEventListener("click", () => socialIconsLinks.classList.toggle("visibility"));
-    profileButton.addEventListener("click", function(){
-        logoHeader.classList.toggle("visibility");
-        
+    socialIconsButton.addEventListener("click", () => socialIconsLinks.classList.toggle("opacity-on") );
+    profileButton.addEventListener("click", (e) => managementTabs(e, 0) );
+    resumeButton.addEventListener("click", (e) => managementTabs(e, 1) );
+    protfolioButton.addEventListener("click", (e) => managementTabs(e, 2) );
+    contactButton.addEventListener("click", (e) => managementTabs(e, 3) );
+}
+    function managementTabs(event, index){
+        if(tabActive !== tabs[index]){
+            managementTabActive(event);
+            tabActive = tabs[index];
+            managementTabActive(event);
+        }
+    }
+        function managementTabActive(event){
+            var tabElement = event.target.parentElement.parentElement;
+            // Logo
+            switch(tabActive){
+                case tabs[0]:
+                    if(isLogoVisible){
+                        logoHeader.classList.toggle("translate-xy");
+                        logoHeader.classList.toggle("opacity-on");
+                        isLogoVisible = false;
+                    }
+                break;
+                case tabs[1]:
+                case tabs[2]:
+                case tabs[3]:
+                    if(!isLogoVisible){
+                        logoHeader.classList.toggle("translate-xy");
+                        logoHeader.classList.toggle("opacity-on");
+                        isLogoVisible = true;
+                    }
+                break;
+                default: console.log("ERROR » tab not valid");
+            }
+            // Menu / Content
+            switch(tabActive){
+                case tabs[0]:
+                    profileButton.parentElement.classList.toggle("active");
+                    profileButton.classList.toggle("active");
+                    profileContent.classList.toggle("collapse-height");
+                break;
+                case tabs[1]:
+                    resumeButton.parentElement.classList.toggle("active");
+                    resumeButton.classList.toggle("active");
+                    resumeContent.classList.toggle("display-flex");
+                    setTimeout( () => {
+                        resumeContent.classList.toggle("opacity-on"); }, 250);
+                break;
+                case tabs[2]:
+                    protfolioButton.parentElement.classList.toggle("active");
+                    protfolioButton.classList.toggle("active");
+                    portfolioContent.classList.toggle("display-flex");
+                    setTimeout( () => {
+                        portfolioContent.classList.toggle("opacity-on"); }, 250);
+                break;
+                case tabs[3]:
+                    contactButton.parentElement.classList.toggle("active");
+                    contactButton.classList.toggle("active");
+                    contactContent.classList.toggle("display-flex");
+                    setTimeout( () => {
+                        contactContent.classList.toggle("opacity-on"); }, 250);
+                break;
+                default: console.log("ERROR » tab not valid");
+            }
+        }
+
+function initProgressBars(){
+    //document.styleSheets[4].addRule(".resume-skills-data__line-progressbar::before", "text-align: right;");
+    profileContentProgressBars.forEach( (elem) => {
+        const cssComputedStyle = window.getComputedStyle(elem, "::before");
+        var percentage = cssComputedStyle.getPropertyValue("content");
+        percentage = percentage.substring(1, percentage.length - 2) - 2;
+        elem.style.setProperty("--data-val", percentage + "%");
     });
 }
